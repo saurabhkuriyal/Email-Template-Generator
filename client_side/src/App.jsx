6cb3a05824c17b1f4f2ba9167ce58a9444cb77a0
Ninetales-Publishings <147030239+saurabhkuriyal@users.noreply.github.com>
2025-01-24 23:27:30 +0530

    applying dist

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,12 +1,16 @@
-import Home from "./pages/Home"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
 
 
   return (
     <>
-      
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+        </Routes>
+      </BrowserRouter>
       
     </>
   )
